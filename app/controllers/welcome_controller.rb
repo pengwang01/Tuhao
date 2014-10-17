98988e57533a846ec63b1abb7004fd531c5497df
Peng Wang <randywang001@gmail.com>
2014-10-16 19:59:06 -0700

    Able to search with advanced search form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,13 @@ class WelcomeController < ApplicationController
 	@posts 
 	def index
 		if params[:search] then
-			@posts = Post.find(:all, :conditions => ['sendFrom LIKE ?', "%#{params[:search]}%"])
+			@posts = Post.find(:all, 
+				:conditions => ['sendFrom LIKE ?' , "%#{params[:search]}%"])
+		else
+			@posts = Post.find(:all, 
+				:conditions => ['sendFrom LIKE ? and sendTo LIKE ?' , "%#{params[:from]}%", "%#{params[:to]}%"])
 		end	
+
 	end
 
 end
